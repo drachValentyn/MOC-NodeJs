a7d331d5dc210f868ade63248b08167776d03042
valentynd91@gmail.com <valentynd91@gmail.com>
2020-11-03 19:03:02 +0200

    add task1

--- homework-01/src/task/task1.js
+++ homework-01/src/task/task1.js
@@ -1,71 +1,84 @@
 const arrExample = [
   {
-    type: "socks",
-    color: "red",
+    type: 'socks',
+    color: 'red',
     quantity: 10,
-    priceForPair: "$3",
+    priceForPair: '$3',
   },
   {
-    type: "socks",
-    color: "green",
+    type: 'socks',
+    color: 'green',
     quantity: 5,
-    priceForPair: "$10",
+    priceForPair: '$10',
   },
   {
-    type: "socks",
-    color: "blue",
+    type: 'socks',
+    color: 'blue',
     quantity: 8,
-    priceForPair: "$6",
+    priceForPair: '$6',
   },
   {
-    type: "hat",
-    color: "red",
+    type: 'hat',
+    color: 'red',
     quantity: 7,
-    price: "$5",
+    price: '$5',
   },
   {
-    type: "hat",
-    color: "green",
+    type: 'hat',
+    color: 'green',
     quantity: 0,
-    price: "$6",
+    price: '$6',
   },
-  { type: "socks", color: "blue", priceForPair: "$6" },
+  { type: 'socks', color: 'blue', priceForPair: '$6' },
   {
-    type: "socks",
-    color: "red",
+    type: 'socks',
+    color: 'red',
     quantity: 10,
-    priceForPair: "$3",
+    priceForPair: '$3',
   },
   {
-    type: "socks",
-    color: "white",
+    type: 'socks',
+    color: 'white',
     quantity: 3,
-    priceForPair: "$4",
+    priceForPair: '$4',
   },
-  { type: "socks", color: "green", priceForPair: "$10" },
+  { type: 'socks', color: 'green', priceForPair: '$10' },
   {
-    type: "socks",
-    color: "blue",
+    type: 'socks',
+    color: 'blue',
     quantity: 2,
-    priceForPair: "$6",
+    priceForPair: '$6',
   },
   {
-    type: "hat",
-    color: "green",
+    type: 'hat',
+    color: 'green',
     quantity: 3,
-    price: "$5",
+    price: '$5',
   },
   {
-    type: "hat",
-    color: "red",
+    type: 'hat',
+    color: 'red',
     quantity: 1,
-    price: "$6",
+    price: '$6',
   },
-  { type: "socks", color: "blue", priceForPair: "$6" },
+  { type: 'socks', color: 'blue', priceForPair: '$6' },
 ];
 
-function filterArray(arr = [], param = '', value = '') {
-  return arr;
-}
+const filterArray = function (arr = [], param = '', value = '') {
+  const quantity = [];
+  arr.forEach((item) => {
+    Object.keys(item).forEach((e) => {
+      if (e === param) {
+        Object.values(item).forEach((el) => {
+          if (el === value) {
+            quantity.push(item);
+          }
+        });
+      }
+    });
+  });
 
-console.log(filterArray('aaaa'));
+  return quantity;
+};
+
+module.exports = filterArray(arrExample, 'color', 'red');
