6ba59ebb88a5da355dbe9d69e9c4b7c74cf38c2f
valentynd91@gmail.com <valentynd91@gmail.com>
2020-11-20 19:12:41 +0200

    work on t2

--- src/task/index.js
+++ src/task/index.js
@@ -1,5 +1,6 @@
 const task1 = require('./task1');
 const task2 = require('./task2');
 const task3 = require('./task3');
+require('./myMap');
 
 module.exports = { task1, task2, task3 };
--- src/task/myMap.js
+++ src/task/myMap.js
@@ -1,6 +1,21 @@
 // eslint-disable-next-line no-extend-native
-Array.prototype.myMap = function (callback) {
-  const arr = [];
-  for (let i = 0; i < this.length; i++) arr.push(callback(this[i], i, this));
-  return arr;
+// Array.prototype.myMap = function (callback) {
+//   const arr = [];
+//   for (let i = 0; i < this.length; i++) arr.push(callback(this[i], i, this));
+//   return arr;
+// };
+const getRandom = () => {
+  return Math.floor(Math.random() * 100);
 };
+const randomDiscount = (cb) => {
+  setTimeout(() => {
+    const discount = getRandom();
+    if (discount >= 20 && discount !== 0) return cb(new Error(`number too big ${discount}`));
+    return cb(null, discount);
+  }, 50);
+};
+
+randomDiscount((err, number) => {
+  if (err) console.log(err);
+  console.log(number);
+});
